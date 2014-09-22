d9c25fa1337e33a6f1ba39a520cd1fe5e9930500
Lavajumper <lavajumper@lavajumper.com>
2014-09-21 21:11:44 -0400

    change cert passwd; Promotion F&R

--- sample-integration-android/src/de/schildbach/wallet/integration/sample/SampleActivity.java
+++ sample-integration-android/src/de/schildbach/wallet/integration/sample/SampleActivity.java
@@ -35,7 +35,7 @@ import de.schildbach.wallet.integration.android.BitcoinIntegration;
  */
 public class SampleActivity extends Activity
 {
-	private static final String DONATION_ADDRESS = "LPwViUHsXXKWp65ZxVqag7B3wmbmo4Zfd2"; // prodnet (Litecoin Android 2014 Fund)
+	private static final String DONATION_ADDRESS = "S7NgcaY5qtjsBpNqdJsYbeTjacwuCUhC2Z"; // prodnet (Litecoin Android 2014 Fund)
 	// private static final String DONATION_ADDRESS = "mwEacn7pYszzxfgcNaVUzYvzL6ypRJzB6A"; // testnet
 	private static final int REQUEST_CODE = 0;
 
